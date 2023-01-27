fcba62cbbce64ef08aa571242f5e860b08e6740c
Ceduran1692 <carlosdura1692@gmail.com>
2023-01-27 14:45:23 -0300

    creacion proyecto

---
+++ src/main/java/com/bcz_challenge/socialBesy/SocialBesyApplication.java
@@ -0,0 +1,13 @@
+package com.bcz_challenge.socialBesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBesyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcz_challenge/socialBesy/SocialBesyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcz_challenge.socialBesy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBesyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

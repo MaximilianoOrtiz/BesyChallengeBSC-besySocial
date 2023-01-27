ef35c0d530ed02fcbf17b5cc1c09be2bf69f8093
Ceduran1692 <carlosdura1692@gmail.com>
2023-01-27 16:48:37 -0300

    update

--- src/main/java/com/bczchallenge/socialbesy/config/SpringFoxSwagger.java
+++ src/main/java/com/bczchallenge/socialbesy/config/SpringFoxSwagger.java
@@ -20,7 +20,7 @@ public class SpringFoxSwagger {
     public Docket getDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com/bcz_challenge/socialBesy/controller"))
+                .apis(RequestHandlerSelectors.basePackage("com/bczchallenge/socialbesy/controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());

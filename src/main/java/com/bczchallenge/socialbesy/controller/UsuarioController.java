b61c22a5b740230f52fd34d5a4fbb7b1d325bba0
Ceduran1692 <carlosdura1692@gmail.com>
2023-01-29 19:51:06 -0300

    [Develop][UsuarioController] estructura base del response y manejo de excepciones

--- src/main/java/com/bczchallenge/socialbesy/controller/UsuarioController.java
+++ src/main/java/com/bczchallenge/socialbesy/controller/UsuarioController.java
@@ -4,28 +4,81 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     @PostMapping("/{userID}/seguir/{userIDASeguir}")
     ResponseEntity seguir(@PathVariable("userID") Integer userID, @PathVariable("userIDASeguir")Integer userIDASeguir){
-        return null;
+        try{
+            Map<String, Object> mensaje= new HashMap<String, Object>();
+
+            Integer data= null;//TODO LLAMADA A LA IMPLEMENTACION;
+            mensaje.put("Success", true);
+            mensaje.put("Data", data);
+
+            return ResponseEntity.ok(mensaje);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @GetMapping("/{userID}/seguidores/listado")
     ResponseEntity quienMeSigue(@PathVariable(value= "id") Integer id){
-        return null;
+        try{
+            Map<String, Object> mensaje= new HashMap<String, Object>();
+
+            Integer data= null;//TODO LLAMADA A LA IMPLEMENTACION;
+            mensaje.put("Success", true);
+            mensaje.put("Data", data);
+
+            return ResponseEntity.ok(mensaje);
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @GetMapping("/{userID}/seguidores/listado?ordenar=?&desc=?")
     ResponseEntity quienMeSigue(@PathVariable("idUser") Integer idUser, @PathVariable("ordenar") String ordenar, @PathVariable("desc")String desc){
-        return null;
+        try{
+            Map<String, Object> mensaje= new HashMap<String, Object>();
+
+            Integer data= null;//TODO LLAMADA A LA IMPLEMENTACION;
+            mensaje.put("Success", true);
+            mensaje.put("Data", data);
+
+            return ResponseEntity.ok(mensaje);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @GetMapping("/{userID}/sigo/listado?ordenar=?&desc=?")
     ResponseEntity aQuienSigo(@PathVariable("idUser") Integer idUser, @PathVariable("ordenar") String ordenar, @PathVariable("desc")String desc){
-        return null;
+        try{
+            Map<String, Object> mensaje= new HashMap<String, Object>();
+
+            Integer data= null;//TODO LLAMADA A LA IMPLEMENTACION;
+            mensaje.put("Success", true);
+            mensaje.put("Data", data);
+
+            return ResponseEntity.ok(mensaje);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @GetMapping("/{userID}/sigo/listado")
     ResponseEntity aQuienSigo(@PathVariable(value= "id") Integer id){
-        return null;
+        try{
+            Map<String, Object> mensaje= new HashMap<String, Object>();
+
+            Integer data= null;//TODO LLAMADA A LA IMPLEMENTACION;
+            mensaje.put("Success", true);
+            mensaje.put("Data", data);
+
+            return ResponseEntity.ok(mensaje);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }

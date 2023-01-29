4b35edea27a9dc5038c115822a930749f6f43f9c
Ceduran1692 <carlosdura1692@gmail.com>
2023-01-29 14:16:13 -0300

    [Develop][ProductoController] definicion endpoints

--- src/main/java/com/bczchallenge/socialbesy/controller/ProductoController.java
+++ src/main/java/com/bczchallenge/socialbesy/controller/ProductoController.java
@@ -1,4 +1,30 @@
 package com.bczchallenge.socialbesy.controller;
 
+import com.bczchallenge.socialbesy.domain.models.Publicacion;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producto")
 public class ProductoController {
+
+    @PostMapping("/publicar")
+    ResponseEntity publicar(@RequestBody Publicacion publicacion){
+        return null;
+    }
+
+    @GetMapping("/{userID}/listado")
+    ResponseEntity listado(@PathVariable("userID") Integer userId){
+        return null;
+    }
+
+    @GetMapping("/promocion")
+    ResponseEntity promocion(){
+        return null;
+    }
+
+    @GetMapping("/promoción/{userID}/listado")
+    ResponseEntity promocion(@PathVariable("userId")Integer userId){
+        return null;
+    }
 }
